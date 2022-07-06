43da99d254aac2a8add0050572d05ecd316ab0e6
hamza <mograbi.ha@gmail.com>
2022-07-06 10:57:32 +0300

    test file

---
+++ test_base_route_hello_world.py
@@ -0,0 +1,39 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver import Keys
+import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+
+
+@pytest.fixture()
+def driver():
+
+    # chrome_options = Options()
+    # chrome_options.add_argument("--headless")
+
+    dc = {
+            "browserName": "chrome",
+            "platformName": "Windows 11"
+
+         }
+
+    # selenium grid standAlone headless
+    # driver = webdriver.Remote("http://localhost:4444",desired_capabilities= dc,options=chrome_options)
+
+    #selenium grid standAlone
+    driver = webdriver.Remote("http://localhost:4444", desired_capabilities=dc)
+
+
+
+
+    yield driver
+    driver.close()
+
+
+def test_message(driver):
+    driver.get('http://localhost:5000/')
+    time.sleep(3)
+    message=driver.find_element(By.CSS_SELECTOR,"body").text
+    assert  message== "Welcome to the Application with updated code!"
